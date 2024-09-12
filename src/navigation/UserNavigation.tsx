2b5db757ba266f4bd744cebc83b064b95eb39a6d
rimbaxo <rimbaxo@gmail.com>
2024-09-12 17:04:53 +0200

    feat: finish navigation

--- src/navigation/UserNavigation.tsx
+++ src/navigation/UserNavigation.tsx
@@ -1,17 +1,108 @@
+import { BlurView } from '@react-native-community/blur';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Colors } from '../constants';
 import HomeScreen from '../views/HomeScreen';
 import UserInfoScreen from '../views/UserInfoScreen';
 
-const UserNavigation = () => {
-  const Tab = createBottomTabNavigator();
+const Tab = createBottomTabNavigator();
+
+const BlurBackground = ({ screenName }) => {
+  const sstyle = StyleSheet.create({ borderColor: Colors.darkRose, borderWidth: 2 });
 
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-      <Tab.Screen name="UserInfo" component={UserInfoScreen} options={{ headerShown: false }} />
+    <View style={[styles.blurContainer, screenName === 'UserInfo' ? sstyle : {}]}>
+      <BlurView
+        style={[StyleSheet.absoluteFill]}
+        blurType="dark"
+        blurAmount={4}
+        reducedTransparencyFallbackColor="white"
+      />
+    </View>
+  );
+};
+
+const UserNavigation = () => {
+  return (
+    <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarBackground: () => <BlurBackground screenName={route.name} />,
+        tabBarStyle: styles.tabBarStyle,
+        tabBarItemStyle: styles.tabBarItemStyle,
+        tabBarLabelStyle: styles.tabBarLabel,
+        tabBarIconStyle: styles.tabBarIcon,
+        tabBarActiveTintColor: Colors.azure,
+        tabBarInactiveTintColor: Colors.light
+      })}
+    >
+      <Tab.Screen
+        name="Home"
+        component={HomeScreen}
+        options={{
+          headerShown: false
+        }}
+      />
+      <Tab.Screen
+        name="UserInfo"
+        component={UserInfoScreen}
+        options={{
+          headerShown: false
+        }}
+      />
     </Tab.Navigator>
   );
 };
+
+const styles = StyleSheet.create({
+  tabBarStyle: {
+    flex: 1,
+    borderTopWidth: 0,
+    borderColor: Colors.darkRose,
+    borderTopColor: Colors.darkRose,
+    paddingBottom: 0,
+    paddingTop: 0,
+    position: 'absolute',
+    justifyContent: 'center',
+    alignItems: 'center',
+    left: 20,
+    right: 20,
+    bottom: 30,
+    height: 60
+  },
+  tabBarLabel: {
+    fontSize: 18,
+    textAlignVertical: 'center',
+    fontWeight: 'bold',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  tabBarIcon: {
+    display: 'none'
+  },
+  tabBarItemStyle: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  blurContainer: {
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    right: 0,
+    bottom: 0,
+    borderRadius: 30,
+    overflow: 'hidden'
+  },
+  blurView: {
+    flex: 1,
+    borderRadius: 30
+  },
+  blurViewBorders: {
+    borderColor: Colors.darkRose,
+    borderWidth: 2,
+    borderRadius: 30
+  }
+});
 
 export default UserNavigation;
